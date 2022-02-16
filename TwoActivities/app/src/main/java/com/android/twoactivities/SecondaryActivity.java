9eb844479d21ca936a58f8b492fc2dbb39300638
Sergiogonzalezpi <sergio.gonzalezpi@alumnos.upm.es>
2022-02-16 12:52:29 +0100

    Add lifecycle callbacks

--- TwoActivities/app/src/main/java/com/android/twoactivities/SecondaryActivity.java
+++ TwoActivities/app/src/main/java/com/android/twoactivities/SecondaryActivity.java
@@ -1,6 +1,7 @@
 package com.android.twoactivities;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class SecondaryActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.android.twoactivities.extra.REPLY";
     private EditText mReply;
+    private static final String LOG_TAG = SecondaryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class SecondaryActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
